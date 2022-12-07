9a17643ca7ceac0c73dea5bdc2b4483b0691b823
test1 <georgi.tinchev.124@gmail.com>
2022-12-07 14:48:33 +0000

    added a few commits to the app and general styling of the page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,40 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 143.12,
+      date: new Date(2022, 11, 8),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 128.23,
+      title: "",
+      date: new Date(2022, 11, 7),
+    },
+    {
+      id: "e3",
+      title: "House Insurance",
+      amount: 987.14,
+      date: new Date(2022, 11, 7),
+    },
+    {
+      id: "e4",
+      title: "Medical Insurance",
+      amount: 112.12,
+      date: new Date(2022, 11, 7),
+    },
+  ];
+
   return (
     <div>
       <h2 className="starter-header">Let's get started!</h2>
+      <ExpenseItem title="Toilet Paper"></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 11, 7);
+  const expenseTitle = "Car Insurance";
+  const expenseAmntVariable = 304.2;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmntVariable}</div>
       </div>
     </div>
   );
